e11d8595bfb446357b358292d97929ebc7e27216
“fariahatulmaria” <89478001+farihatulmaria@users.noreply.github.com>
2022-03-18 18:23:08 +0600

    update

--- 01-birthday-reminder/src/App.js
+++ 01-birthday-reminder/src/App.js
@@ -1,9 +1,17 @@
 import React, { useState } from 'react';
-import List from './Components/List/List.jsx';
-import data from './data.jsx';
+import List from './Components/List/List';
+import data from './data';
 function App() {
-  const [first, setfirst] = useState(0)
-  return <h2>reminder project setup{data} {List}</h2>;
+  const [people, setPeople] = useState(data);
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        <List people = {people}/>
+        <button onClick={() => setPeople([])}>clear all</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
