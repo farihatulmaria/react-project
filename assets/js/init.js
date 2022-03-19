3f0205ae115aa1b76bb5441d913e3fd507d72f82
“fariahatulmaria” <89478001+farihatulmaria@users.noreply.github.com>
2022-03-19 16:57:56 +0600

    adding all the projects
    adding all the projects in the root index html file dynamically

---
+++ assets/js/init.js
@@ -0,0 +1,34 @@
+// dom elements
+const projectContainer = document.querySelector('.project-container .row');
+
+// fetching the data
+const url = '/assets/data/data.json';
+
+const loadData = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayData(data.AllReactProject);
+};
+const displayData = (allData) =>{
+    allData.forEach((data) => {
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+            <div class="card h-100">
+                <div class="card-head">
+                    <img
+                    src="${data.image}"
+                    class="card-img-top"
+                    alt="${data.title}"
+                    />
+                </div>
+                <div class="card-body text-center">
+                    <h5 class="card-title">${data.title}</h5>
+                </div>
+            </div>
+        `
+        projectContainer.appendChild(div);
+    });
+}
+
+loadData()
