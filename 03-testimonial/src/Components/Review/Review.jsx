0512fbaf61ab8961439306c251efe3e619885fea
“fariahatulmaria” <89478001+farihatulmaria@users.noreply.github.com>
2022-03-19 18:02:53 +0600

    update

--- 03-testimonial/src/Components/Review/Review.jsx
+++ 03-testimonial/src/Components/Review/Review.jsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import people from '../data/data';
 import './Review.css';
+
+
 const Review = () => {
+    const [index, setIndex] = useState(0);
+    const {name,job,image,text} = people[index];
     return (
-        <div>
-            
-        </div>
+        <article className='review'>
+            <div className="img-container">
+                <img src={image} alt={name} className ="person-img"/>
+                <span className='quote-icon'>
+                    <FaQuoteRight/>
+                </span>
+            </div>
+            <h4 className="author">{name}</h4>
+            <p className="job">{job}</p>
+            <p className="info">{text}</p>
+            <div className="button-contaienr">
+                <button className='prev-btn'>
+                    <FaChevronLeft/>
+                </button>
+                <button className='next-btn'>
+                    <FaChevronRight/>
+                </button>
+            </div>
+            <button className='random-btn'>suprise me</button>
+        </article>
     );
 };
+
+
 
 export default Review;
