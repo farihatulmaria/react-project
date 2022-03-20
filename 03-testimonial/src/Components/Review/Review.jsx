b4735a190b789520ec12a313aa52965dea65138d
“fariahatulmaria” <89478001+farihatulmaria@users.noreply.github.com>
2022-03-20 17:06:38 +0600

    completed 3-testimonial repo

--- 03-testimonial/src/Components/Review/Review.jsx
+++ 03-testimonial/src/Components/Review/Review.jsx
@@ -7,6 +7,36 @@ import './Review.css';
 const Review = () => {
     const [index, setIndex] = useState(0);
     const {name,job,image,text} = people[index];
+    const checkTheNumber = (num) => {
+        if(num > people.length -1){
+            return 0;
+        }
+        if(num < 0){
+            return people.length -1;
+        }
+        return num;
+    }
+    const prevPerson = () => {
+        setIndex((index) => {
+            let newIndex = index - 1
+            return checkTheNumber(newIndex) ;
+        });
+    }
+    const nextPerson = () => {
+        setIndex((index) => {
+            let newIndex = index + 1
+            return checkTheNumber(newIndex) ;
+        });
+    }
+
+const randomPerson = () => {
+    let randomNum = Math.floor(Math.random() * people.length);
+    if(randomNum === index){
+        randomNum = index + 1;
+    }
+    setIndex(checkTheNumber(randomNum))
+}
+
     return (
         <article className='review'>
             <div className="img-container">
@@ -18,15 +48,15 @@ const Review = () => {
             <h4 className="author">{name}</h4>
             <p className="job">{job}</p>
             <p className="info">{text}</p>
-            <div className="button-contaienr">
-                <button className='prev-btn'>
+            <div className="button-container">
+                <button className='prev-btn' onClick={prevPerson}>
                     <FaChevronLeft/>
                 </button>
-                <button className='next-btn'>
+                <button className='next-btn' onClick={nextPerson}>
                     <FaChevronRight/>
                 </button>
             </div>
-            <button className='random-btn'>suprise me</button>
+            <button className='random-btn' onClick={randomPerson}>suprise me</button>
         </article>
     );
 };
