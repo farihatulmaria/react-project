cff58057075d99e100cd73aeb980449076446c39
“fariahatulmaria” <89478001+farihatulmaria@users.noreply.github.com>
2022-03-20 17:51:29 +0600

    little update of accordion

--- 04-accordion/src/Components/Question/Question.jsx
+++ 04-accordion/src/Components/Question/Question.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 const Question = ({title,info}) => {
-    const [showInfo, setshowInfo] = useState(false);
+    const [showInfo, setShowInfo] = useState(false);
 
     return (
         <article className='question'>
             <header>
                 <h4>{title}</h4>
-                <button className='btn' onClick={() =>setshowInfo(!showInfo)}>
+                <button className='btn' onClick={() =>setShowInfo(!showInfo)}>
                     {
                         showInfo? <AiOutlineMinus/> : <AiOutlinePlus/>
                     }
